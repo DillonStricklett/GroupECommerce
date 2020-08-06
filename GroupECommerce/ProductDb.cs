f79167caf287f86a22bccb8d6a72f20f73293317
Dillon Stricklett <dillonstricklett@live.com>
2020-08-06 00:38:43 -0700

    Created database class with unfinished CRUD functions.

---
+++ GroupECommerce/ProductDb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupECommerce
+{
+    class ProductDb
+    {
+        public static List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Product Add(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Product Update(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Delete(Product p)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
