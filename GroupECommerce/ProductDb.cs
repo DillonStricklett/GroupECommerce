e0e303c3a6885cee8093c98fed4ee8d0b007a95c
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-06 19:01:47 -0700

    Implemented Delete methods and commented code

--- GroupECommerce/ProductDb.cs
+++ GroupECommerce/ProductDb.cs
@@ -9,6 +9,10 @@ namespace GroupECommerce
 {
     static class ProductDb
     {
+
+        /// <summary>
+        /// Collects all products from database and returns them in a list
+        /// </summary>
         public static List<Product> GetAllProducts()
         {
             using (var context = new ProductContext())
@@ -17,6 +21,10 @@ namespace GroupECommerce
 			}
         }
 
+        /// <summary>
+        /// Adds a product to the database
+        /// </summary>
+        /// <param name="p">The product being added</param>
         public static Product Add(Product p)
         {
             using (var context = new ProductContext())
@@ -27,6 +35,10 @@ namespace GroupECommerce
 			}
         }
 
+        /// <summary>
+        /// Updates the submitted product in the database
+        /// </summary>
+        /// <param name="p">The product to update</param>
         public static Product Update(Product p)
         {
             using (var context = new ProductContext())
@@ -37,14 +49,35 @@ namespace GroupECommerce
 			}
         }
 
+        /// <summary>
+        /// Deletes product from databse
+        /// </summary>
+        /// <param name="p">The product to be deleted</param>
         public static void Delete(Product p)
         {
-            throw new NotImplementedException();
+            using (var context = new ProductContext())
+			{
+                context.Entry(p).State = EntityState.Deleted;
+                context.SaveChanges();
+			}
         }
         
+        /// <summary>
+        /// Deletes a product from the database
+        /// </summary>
+        /// <param name="id">The id of the product to delete</param>
         public static void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (var context = new ProductContext())
+			{
+                // Collects and marks product for deletion in one statement
+                context.Entry(
+                    context.Products
+                           .Where(p => p.ProductId == id)
+                           .Single())
+                        .State = EntityState.Deleted;
+                context.SaveChanges();
+			}
         }
     }
 }
