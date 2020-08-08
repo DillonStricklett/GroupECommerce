22da1e47ae275831e0bea1566ed0ffb8cbe5880d
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-07 20:53:44 -0700

    Added AddProduct form and renamed Form1 to MainForm

--- GroupECommerce/AddProduct.cs
+++ GroupECommerce/AddProduct.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace GroupECommerce
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class AddProduct : Form
+	{
+		public AddProduct()
+		{
+			InitializeComponent();
+		}
+	}
 }
