d56082a90e98a5c319b8a06390825bc0f8db764c
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-08-07 21:21:47 -0700

    AddProduct form adds to the databse

--- GroupECommerce/AddProduct.Designer.cs
+++ GroupECommerce/AddProduct.Designer.cs
@@ -28,35 +28,35 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.titleBox = new System.Windows.Forms.TextBox();
+			this.priceBox = new System.Windows.Forms.TextBox();
+			this.categoryBox = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
+			this.addProdBtn = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// textBox1
+			// titleBox
 			// 
-			this.textBox1.Location = new System.Drawing.Point(81, 18);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
-			this.textBox1.TabIndex = 0;
+			this.titleBox.Location = new System.Drawing.Point(81, 18);
+			this.titleBox.Name = "titleBox";
+			this.titleBox.Size = new System.Drawing.Size(100, 20);
+			this.titleBox.TabIndex = 0;
 			// 
-			// textBox2
+			// priceBox
 			// 
-			this.textBox2.Location = new System.Drawing.Point(81, 44);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 20);
-			this.textBox2.TabIndex = 1;
+			this.priceBox.Location = new System.Drawing.Point(81, 44);
+			this.priceBox.Name = "priceBox";
+			this.priceBox.Size = new System.Drawing.Size(100, 20);
+			this.priceBox.TabIndex = 1;
 			// 
-			// textBox3
+			// categoryBox
 			// 
-			this.textBox3.Location = new System.Drawing.Point(81, 70);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(100, 20);
-			this.textBox3.TabIndex = 2;
+			this.categoryBox.Location = new System.Drawing.Point(81, 70);
+			this.categoryBox.Name = "categoryBox";
+			this.categoryBox.Size = new System.Drawing.Size(100, 20);
+			this.categoryBox.TabIndex = 2;
 			// 
 			// label1
 			// 
@@ -85,27 +85,28 @@
 			this.label3.TabIndex = 5;
 			this.label3.Text = "Category";
 			// 
-			// button1
+			// addProdBtn
 			// 
-			this.button1.Location = new System.Drawing.Point(92, 96);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 6;
-			this.button1.Text = "Add Product";
-			this.button1.UseVisualStyleBackColor = true;
+			this.addProdBtn.Location = new System.Drawing.Point(92, 96);
+			this.addProdBtn.Name = "addProdBtn";
+			this.addProdBtn.Size = new System.Drawing.Size(75, 23);
+			this.addProdBtn.TabIndex = 6;
+			this.addProdBtn.Text = "Add Product";
+			this.addProdBtn.UseVisualStyleBackColor = true;
+			this.addProdBtn.Click += new System.EventHandler(this.addProdBtn_Click);
 			// 
 			// AddProduct
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(242, 132);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.addProdBtn);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.categoryBox);
+			this.Controls.Add(this.priceBox);
+			this.Controls.Add(this.titleBox);
 			this.Name = "AddProduct";
 			this.Text = "Add Product";
 			this.ResumeLayout(false);
@@ -115,12 +116,12 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox titleBox;
+		private System.Windows.Forms.TextBox priceBox;
+		private System.Windows.Forms.TextBox categoryBox;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button addProdBtn;
 	}
 }
--- GroupECommerce/AddProduct.cs
+++ GroupECommerce/AddProduct.cs
@@ -16,5 +16,38 @@ namespace GroupECommerce
 		{
 			InitializeComponent();
 		}
+
+		private void addProdBtn_Click(object sender, EventArgs e)
+		{
+			string title = titleBox.Text;
+			if (String.IsNullOrWhiteSpace(title))
+			{
+				MessageBox.Show("Title cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (!double.TryParse(priceBox.Text, out double price))
+			{
+				MessageBox.Show("Price must be a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}			
+
+			string category = categoryBox.Text;
+			if (String.IsNullOrWhiteSpace(category))
+			{
+				MessageBox.Show("Category cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			Product product = new Product
+			{
+				Title = title,
+				Price = price,
+				Category = category
+			};
+
+			ProductDb.Add(product);
+			MessageBox.Show("Product was added", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
 	}
 }
